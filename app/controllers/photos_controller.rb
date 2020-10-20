d1ce3186cc0b3378dc421320808e3febb5f80bb5
Arun <arunshrivastavaror@gmail.com>
2020-10-20 15:26:00 +0530

    Create action commented in photos controller.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -29,19 +29,19 @@ class PhotosController < ApplicationController
 
   # POST /photos
   # POST /photos.json
-  def create
-    @photo = Photo.new(photo_params)
+  # def create
+  #   @photo = Photo.new(photo_params)
 
-    respond_to do |format|
-      if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
-        format.json { render :show, status: :created, location: @photo }
-      else
-        format.html { render :new }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @photo.save
+  #       format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+  #       format.json { render :show, status: :created, location: @photo }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @photo.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /photos/1
   # PATCH/PUT /photos/1.json
