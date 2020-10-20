972093abde907cb7e1f3379605742e9109337e83
Arun <arunshrivastavaror@gmail.com>
2020-10-20 16:24:25 +0530

    photos controller modified.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -45,17 +45,17 @@ class PhotosController < ApplicationController
 
   # PATCH/PUT /photos/1
   # PATCH/PUT /photos/1.json
-  def update
-    respond_to do |format|
-      if @photo.update(photo_params)
-        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @photo }
-      else
-        format.html { render :edit }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @photo.update(photo_params)
+  #       format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @photo }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @photo.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /photos/1
   # DELETE /photos/1.json
